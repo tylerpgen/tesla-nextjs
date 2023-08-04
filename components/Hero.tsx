170a46d141864c179704cb8cb7eb46535effdb06
Tyler Gener <tylergen33@gmail.com>
2023-08-04 14:21:04 -0700

    created base hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+
+const Hero = () => {
+
+  const handleScroll = () => {}
+
+  return (
+    <div className='hero'>
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Find YOUR Tesla today!
+            </h1>
+            <p className="hero__subtitle">
+                Finding a Tesla made easy!
+            </p>
+            <CustomButton
+              title='Explore Cars'
+              containerStyles='bg-primary-blue text-white rounded-full mt-10'
+              handleClick={handleScroll}
+            />
+        </div>
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/teslamodels.jpg" alt='hero' fill className='object-contain'  />
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
