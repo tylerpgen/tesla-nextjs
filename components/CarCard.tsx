560cf701d75bb7807de1c8a5c23cfd275bce3c9d
Tyler Gener <tylergen33@gmail.com>
2023-08-30 15:10:53 -0700

    created car rent calculator

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -10,7 +10,20 @@ interface CarCardProps {
 }
 
 const CarCard = ({ car }) => {
-  return <div>CarCard</div>;
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p>
+        <span>Car Rent...</span>
+      </p>
+    </div>
+  );
 };
 
 export default CarCard;
