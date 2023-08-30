3f2625842fed8e0bc55ccc93dedd0e83628d0b97
Tyler Gener <tylergen33@gmail.com>
2023-08-30 15:31:04 -0700

    created useState and imported Custom Button

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -12,6 +12,7 @@ interface CarCardProps {
 
 const CarCard = ({ car }) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
+  const [isOpen, setIsOpen] = useState(false);
 
   const carRent = calculateCarRent(city_mpg, year);
 
@@ -31,7 +32,7 @@ const CarCard = ({ car }) => {
         <Image src="/teslahero.png" alt="car model" fill priority className="object-contain" />
       </div>
 
-      <div className="relative flex w-full mt-2">
+      <div className="relative flex justify-between w-full mx-5 mt-2">
         <div className="flex group-hover:invisible w-full justify-between text-gray">
           <div className="flex flex-col justify-center items-center gap-2">
             <Image src="/steering-wheel.svg" width={20} height={20} alt="steering-wheel" />
@@ -49,6 +50,17 @@ const CarCard = ({ car }) => {
             <Image src="/gas.svg" width={20} height={20} alt="steering-wheel" />
             <p className="text-[14px]">{city_mpg} MPG</p>
           </div>
+        </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => {
+              setIsOpen;
+            }}
+          />
         </div>
       </div>
     </div>
