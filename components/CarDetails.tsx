0ddfa5bf4cd973e44b09b9880203a6e9ac55e081
Tyler Gener <tylergen33@gmail.com>
2023-09-01 09:42:47 -0700

    created image panels in modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -39,10 +39,31 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel>
-                  <button type="button" onClick={closeModal}>
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex-col gap-5">
+                  <button
+                    type="button"
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                    onClick={closeModal}
+                  >
                     <Image src="/close.svg" alt="close" width={20} height={20} className="object-contain" />
                   </button>
+
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image src="/teslahero.png" alt="car model" fill priority className="object-contain" />
+                    </div>
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image src="/teslahero.png" alt="car model" fill priority className="object-contain" />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image src="/teslahero.png" alt="car model" fill priority className="object-contain" />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image src="/teslahero.png" alt="car model" fill priority className="object-contain" />
+                      </div>
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
