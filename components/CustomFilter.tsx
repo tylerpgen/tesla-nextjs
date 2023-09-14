9b652e5550fbe48e7cfa970af901af0e7c0c1e05
Tyler Gener <tylergen33@gmail.com>
2023-09-14 15:34:53 -0700

    created filter buttons

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -9,11 +9,19 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
   return (
     <div className="w-fit">
-      <Listbox>
+      <Listbox value={selected} onChange={(e) => setSelected(e)}>
         <div className="relative w-fit z-10">
-          <Listbox.Button className="custom-bilter__btn">
-            <span>Filter</span>
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron up down"
+            />
           </Listbox.Button>
+          <Transition></Transition>
         </div>
       </Listbox>
     </div>
