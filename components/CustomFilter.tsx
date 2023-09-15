40cc3f2d7e488566bfc30c4b1161e8143049afe6
Tyler Gener <tylergen33@gmail.com>
2023-09-15 13:03:49 -0700

    created reusable updateSearchParam func

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -6,7 +6,15 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (type: string, value: string) => {
+    const newPathName = "";
+
+    router.push(newPathname, { scroll: false });
+  };
+
   return (
     <div className="w-fit">
       <Listbox value={selected} onChange={(e) => setSelected(e)}>
